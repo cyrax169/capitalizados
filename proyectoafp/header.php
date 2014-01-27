a684a4f6a2a9d95b67c4bdd0cc3687c10c5e7cfa
claudiadela7 <claudiadela7@gmail.com>
2014-01-27 13:04:44 +0000

    menu completo

--- proyectoafp/header.php
+++ proyectoafp/header.php
@@ -2,9 +2,8 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Capitalizados</title>
         <link rel="stylesheet" href="css/estilos2.css" type="text/css" />
-        <a href=""><img src="imagenes/banner.png" border="0" width="928" height="90" /></a>
+        <a href=""><img src="imagenes/banner.png" border="0"  height="90" /></a>
                
     </head>
    
