6a7f002f3573611be9e96e643351ddbd9211d7d1
claudiadela7 <claudiadela7@gmail.com>
2014-02-14 17:02:52 +0000

    se insertan nombres de vistas que faltan por hacer separadas por usuario y administrador

---
+++ proyectoafp/usuarios/menu_usuario.php
@@ -0,0 +1,27 @@
+
+<div id='menuWrapper'>
+    <div class='menu'>
+          <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Quienes Somos</a></li>
+          <li><a href="#">Nuestros Servicios</a>
+          <ul>
+          <li><a href="URL del enlace">Porqué inclinarse por nuestra asesoría</a></li>
+          <li><a href="URL del enlace">Servicios ofrecidos</a></li>
+          <li><a href="URL del enlace">Beneficios adicionales de una estrategia activa</a></li>
+          <li><a href="URL del enlace">Procedimiento de calculos de honorarios de la asesoría</a></li>
+          <li><a href="URL del enlace">Información relevante para los potenciales clientes</a></li>
+          </ul>
+          </li>
+          <li><a href="ficha.php">Mis Datos</a></li>
+          <li2><a href="subir_cartola.php">Subir Cartola</a></li2>
+          <li2><a href="#">Contacto</a></li2>
+
+          </ul>
+
+
+
+
+        </div>
+       </div>
+        
