2881f34543b45eb3c60426bd1e174427277ab324
claudiadela7@gmail.com <claudiadela7@gmail.com@dadba06a-a8da-258c-a08a-cfbe5ec56a57>
2014-02-14 17:02:52 +0000

    se insertan nombres de vistas que faltan por hacer separadas por usuario y administrador

---
+++ trunk/proyectoafp/usuarios/principal.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Usuario Registrado</title>
+         <?php
+              include('header.php');
+         ?>
+    </head>
+    <body>
+        <div id="page-wrap">
+        <div id="contenedor">  
+                <?php
+                    include('menu.php');
+                ?>
+
+       </div>
+          
+
+            </div>
+        <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>
+   </body>
+</html>
