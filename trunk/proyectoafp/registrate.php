88ba60a5f9bdfd5c2329b19187eb3af2e9f7824a
claudiadela7@gmail.com <claudiadela7@gmail.com@dadba06a-a8da-258c-a08a-cfbe5ec56a57>
2014-02-16 00:29:18 +0000

    validacion de campos

--- trunk/proyectoafp/registrate.php
+++ trunk/proyectoafp/registrate.php
@@ -19,6 +19,49 @@ include("conexion.php");
          <?php
               include('header.php');
          ?>
+<script type="text/javascript">
+function valida(f) {
+    
+  var ok = true;
+  var msg = "Debes escribir algo en los campos:\n";
+  if(f.elements[0].value == "")
+  {
+     
+    msg += "-rut\n";
+    ok = false;
+  }
+
+  if(f.elements["nombre1"].value == "")
+  {
+   msg += "- nombre1\n";
+    ok = false;
+  }
+
+  if(f.elements["nombre2"].value == "")
+  {
+    msg += "- nombre2\n";
+    ok = false;
+  }
+  
+  if(f.elements["apellido1"].value == "")
+  {
+   msg += "- primer apellido\n";
+    ok = false;
+  }
+
+  if(f.elements["apellido2"].value == "")
+  {
+    msg += "- segundo apellido\n";
+    ok = false;
+  }
+
+  if(ok == false)  {
+       
+    alert(msg);
+    }
+  return ok;
+}
+    </script>
     </head>
     <body>
          <div id="page-wrap">
@@ -35,38 +78,38 @@ include("conexion.php");
                 </thead>
                  <tbody>
                     <tr>
-                         <td height="350" width="500"></td>
-                         <td height="350" width="700">
-<FORM action="insertarregistro.php" method="post"> 
+                         <td height="500" width="500"></td>
+                         <td height="450" width="700">
+                             <FORM id="miForm" action="insertarregistro.php" method="post" onsubmit="return valida(this)"> 
 <h1></h1>  
-<table border="0" align="center" bgcolor="#D0D0D0" >
+<table border="0" align="top" bgcolor="#D0D0D0" >
                <thead>
                    <tr>
-                       <th height="65"><font color="#CC0000">Datos de identificación</font></th>
+                       <th height="30"><font color="#CC0000">Datos de identificación</font></th>
                        <th></th>
                    </tr>
                </thead>
                <tbody>
                    <tr>
-                       <td  height="38"><font color="#666666">Rut<nt/fo></td>
+                       <td  height="35"><font color="#666666">Rut<font/></td>
                        <td><input type="text" name="rut" maxlength="10"/><font color="#666666">Ej: 12345678-9</font></td>
                        
                    </tr>
                    
                    <tr>
-                       <td height="38"><font color="#666666">Nombres</font></td>
+                       <td height="35"><font color="#666666">Nombres</font></td>
                        <td><input type="text" name="nombre1"/><input type="text" name="nombre2"/></td>
                    </tr>
                    <tr>
-                       <td height="38"><font color="#666666">Apellidos</font></td>
+                       <td height="35"><font color="#666666">Apellidos</font></td>
                        <td><input type="text" name="apellido1"/><input type="text" name="apellido2"/></td>
                    </tr>
                    <tr>
-                       <td height="38"><font color="#666666">Fecha nacimiento</font></td>
+                       <td height="35"><font color="#666666">Fecha nacimiento</font></td>
                        <td><input type="date" name="fecha_nacimiento"/></td>
                    </tr>
                     <tr>
-                       <td height="38"><font color="#666666">Tipo Cliente</font></td>
+                       <td height="35"><font color="#666666">Tipo Cliente</font></td>
                        <td><select name="tipo_cliente">
                                <option VALUE="0">Seleccione tipo</option>
                                <option VALUE="1">Trabajador Activo</option>
@@ -74,7 +117,7 @@ include("conexion.php");
                            </select></td>
                    </tr>
                    <tr>
-                       <td height="38"><font color="#666666">Región de Residencia</font></td>
+                       <td height="35"><font color="#666666">Región de Residencia</font></td>
                        <td><select name="region_residencia">
                               <?php  $result = mysqli_query($conn,"SELECT  id, nombre FROM regiones");
     ;
@@ -83,12 +126,12 @@ include("conexion.php");
                     </select></td>
                    </tr>
                    <tr>
-                       <td height="38"><font color="#666666">Genero</font></td>
+                       <td height="35"><font color="#666666">Genero</font></td>
                        <td><input type="radio" name="genero" value="1" checked onClick=""> <font color="#666666">Femenino</font>
 <input type="radio" name="genero" value="2" onClick=""><font color="#666666">Masculino</font> </td>    
                    </tr>
                    <tr>
-                       <td height="38"><font color="#666666">AFP en que se encuentra</font></td>
+                       <td height="35"><font color="#666666">AFP en que se encuentra</font></td>
                        <td><select name="afp">
                         <?php        $result2 = mysqli_query($conn,"SELECT  id, nombre FROM afp");
                              while ($row2=mysqli_fetch_array($result2)){?> <OPTION VALUE="<?php echo $row2['id'] ?>"><?php echo $row2['nombre'] ?></OPTION><?php } ?> 
@@ -99,13 +142,12 @@ include("conexion.php");
                        <td><input type="file" name="cartola" value="" /></td>
                    </tr>
                    <tr>
-                       <td height="38"><font color="#666666">Certificado Saldo Cuenta</font></td>
+                       <td height="30"><font color="#666666">Certificado Saldo Cuenta</font></td>
                        <td><input type="file" value=""  name="certificado" /></td>
                    </tr>
-                   
-                    <tr>
-                       <td height="38"> </td>
-                       <td><input  type="submit" value="Registrar" name="registrar" style='width:170px; height:25px' /></td>
+                   <tr>
+                       <td height="30"> </td>
+                       <td><input type="submit" value="Registrar" name="registrar" style='width:170px; height:25px' /></td>
                    </tr>
                   
                </tbody>
