a9440e6e685befd0bf361bd6a320aa344551daac
cyrax169@gmail.com <cyrax169@gmail.com@dadba06a-a8da-258c-a08a-cfbe5ec56a57>
2014-03-03 21:47:47 +0000

    creación de nuestros servicios, intento de subida de cartola y certificado

---
+++ trunk/proyectoafp/nuestrosServicios.php
@@ -0,0 +1,93 @@
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+         <?php
+              include('header.php');
+         ?>
+    </head>
+    <body>
+        <div id="page-wrap">
+        <div id="contenedor">  
+                <?php
+                    include('menu.php');
+                ?>
+
+       </div>
+            <CENTER>
+            <table  border="0">
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody align="center">
+                    <tr ><td height="300" width="350"><table border="0" align="center" bgcolor='#DFD5FF'>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                <li><a href="">POR QUÉ INCLINARSE POR NUESTRA ASESORÍA</a></li>
+                                <li><a href="">SERVICIOS OFRECIDOS</a></li>
+                                <li><a href="">BENEFICIOS ADICIONALES DE UNA ESTRATÉGIA ACTIVA</a></li>
+                                <li><a href="">PROCEDIMIENTO DE CÁLCULO DE LOS HONORARIOS DE LA ASESORÍA</a></li>
+                                <li><a href="">INFORMACIÓN RELEVANTE PARA LOS POTENCIALES CLIENTES</a></li>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                     </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td height="300" width="380"><table border="0" align="center">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody align="center">
+                                    <tr>
+                                        <td height="100"><img src="imagenes/imagen7.png" border="0" width="300" height="300" align="center"/></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td height="300" width="300"><table align="center" border="0">
+                                <thead>
+                                    <tr>
+                                        <th><font color="#CC0000"> Nuestro equipo </font></th>
+                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td height="230" align="center"><img src="../imagenes/imagen6.png" border="0" width="200" height="200" /></td>
+                                    </tr>
+                                    <tr>
+                                        <td height="230">
+                                            Somos una empresa compuesta por <br>
+                                            profesionales de vasta experiencia en <br>
+                                            materias financieras, que han unido <br>
+                                            sus hablilidades y conocimeintos para <br>
+                                            otorgar un servicio web a sus clientes.<br> 
+                                            Dentro de su plana ejecutiva contamos <br>
+                                            con Ingenieros Comerciales, Contado- <br>
+                                            res Públicos y Auditores, Ingeniero In- <br>
+                                            dustrial, que adicionalmente disponen <br>
+                                            de grados de Magister y Doctor. <br>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                         
+                    </tr>
+                 
+                </tbody>
+            </table>
+</CENTER>
+            </div>
+        <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>
+   </body>
+   
+</html>
---
+++ trunk/proyectoafp/usuarios/nuestrosServicios.php
@@ -0,0 +1,122 @@
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+         <?php
+              include('header.php');
+         ?>
+    </head>
+    <body>
+        <div id="page-wrap">
+        <div id="contenedor">  
+                <?php
+                    include('menu.php');
+                ?>
+
+       </div>
+            <CENTER>
+            <table  border="0">
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody align="center">
+                    <tr ><td height="300" width="350"><table border="0" align="center" bgcolor='#DFD5FF'>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                <li>POR QUÉ INCLINARSE POR NUESTRA ASESORÍA</li>
+                                <li>SERVICIOS OFRECIDOS</li>
+                                <li>BENEFICIOS ADICIONALES DE UNA ESTRATÉGIA ACTIVA</li>
+                                <li>PROCEDIMIENTO DE CÁLCULO DE LOS HONORARIOS DE LA ASESORÍA</li>
+                                <li>INFORMACIÓN RELEVANTE PARA LOS POTENCIALES CLIENTES</li>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                     <td height="160"><table border="0" align="center">
+                             <thead>
+                                 <tr>
+                                     <th><font color="#CC0000">  Visión </font></th>
+                                 </tr>
+                             </thead>
+                             <tbody >
+                                 <tr>
+                                     <td  height="160">
+                                         Esperamos ser reconocidos como una empre-<br>
+                                         sa de profesionales de excelencia, que asiste <br>
+                                         técnicamente a los cotizantes del sistema de <br>
+                                         cotización individual. Pretendemos que en <br>
+                                         funcion de nuestras sugerencias, los clientes <br>
+                                         maximicen la rentabilidad de sus fondos y por <br>
+                                         ende incrementen su patrimonio.<br> 
+                                         Nuestro compromiso permanente, es acompa- <br>
+                                         ñarlo en la búsqueda de mejores espectati- <br>
+                                         vas, en su calidad de vida futura al momen- <br>
+                                         to de pensionarse. <br>
+                                     </td>
+                                     <td></td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                     </td>
+                     </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td height="400" width="280"><table align="center" border="0">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody align="center">
+                                    <tr>
+                                        <td height="200"><img src="../imagenes/imagen4.png" border="0" width="230" height="230" /></td>
+                                    </tr>
+                                    <tr>
+                                        <td height="300" ><img src="../imagenes/imagen5.png" border="0" width="230" height="230" /></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td height="300" width="300"><table align="center" border="0">
+                                <thead>
+                                    <tr>
+                                        <th><font color="#CC0000"> Nuestro equipo </font></th>
+                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td height="230" align="center"><img src="../imagenes/imagen6.png" border="0" width="200" height="200" /></td>
+                                    </tr>
+                                    <tr>
+                                        <td height="230">
+                                            Somos una empresa compuesta por <br>
+                                            profesionales de vasta experiencia en <br>
+                                            materias financieras, que han unido <br>
+                                            sus hablilidades y conocimeintos para <br>
+                                            otorgar un servicio web a sus clientes.<br> 
+                                            Dentro de su plana ejecutiva contamos <br>
+                                            con Ingenieros Comerciales, Contado- <br>
+                                            res Públicos y Auditores, Ingeniero In- <br>
+                                            dustrial, que adicionalmente disponen <br>
+                                            de grados de Magister y Doctor. <br>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                         
+                    </tr>
+                 
+                </tbody>
+            </table>
+</CENTER>
+            </div>
+        <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>
+   </body>
+   
+</html>
