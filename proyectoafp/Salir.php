8dc550eb03978db10d19ca86218d3e3a62a2bbf3
cyrax169 <cyrax169@gmail.com>
2014-03-04 18:44:25 +0000

    Se cambió el código php para cerrar la sesión

--- proyectoafp/Salir.php
+++ proyectoafp/Salir.php
@@ -1,3 +1,10 @@
+<?php
+  session_start();
+  unset($_SESSION["nombre_usuario"]); 
+  session_destroy();
+  header("Location: index.php");
+  exit;
+?>
 <html>
     <head>
         <meta charset="UTF-8">
