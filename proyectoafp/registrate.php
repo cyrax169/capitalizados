34bf3c02db71dfae626d733e93fc6f7a1e5e7e51
claudiadela7 <claudiadela7@gmail.com>
2014-03-05 21:15:58 +0000

    footer cambiado

--- proyectoafp/registrate.php
+++ proyectoafp/registrate.php
@@ -140,6 +140,7 @@ function valida(f) {
 
       </div>
               </div>
-        <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>
+      
    </body>
+     <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>
 </html>
