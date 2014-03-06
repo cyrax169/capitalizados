3e9b8fd934f7ab62e9af866157afb1138d3cb488
claudiadela7 <claudiadela7@gmail.com>
2014-03-06 00:35:39 +0000

    busquepa por afp

---
+++ proyectoafp/Administrador/Busca_afp.php
@@ -0,0 +1,78 @@
+<?php
+include("../conexion.php");
+   // $result = "";
+    $conn = dbConnect();
+    //$combobox;
+
+ ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Registro</title>
+         <?php
+              include('header.php');
+         ?>
+    </head>
+    <body>
+         <div id="page-wrap">
+        <div id="contenedor">  
+                <?php
+                    include('menu_administrador.php');
+                ?>
+      <div>
+           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <a href="../Salir.php">Salir</a>
+       </div>
+  <table border="0" >
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                 <tbody>
+                    <tr>
+                         <td height="350" width="500"></td>
+                         <td height="350" width="700">
+                             <FORM action="Busca_nombre_AFP.php" method="post"> 
+<h1></h1>  
+<table border="0" align="center" bgcolor="#D0D0D0" >
+               <thead>
+                   <tr>
+                       <th height="65"><font color="#CC0000">Busqueda de cliente por AFP</font></th>
+                       <th></th>
+                   </tr>
+               </thead>
+               <tbody>
+                 <tr>
+                       <td height="38"><font color="#666666">seleccione nombre</font></td>
+                       <td><select name="afp">
+                               <option VALUE="0">Seleccione tipo</option>
+                        <?php        $result2 = mysqli_query($conn,"SELECT  id, nombre FROM afp");
+                             while ($row2=mysqli_fetch_array($result2)){?> <OPTION VALUE="<?php echo $row2['id'] ?>"><?php echo $row2['nombre'] ?></OPTION><?php } ?> 
+                    </select></td>
+                   </tr>
+                    
+                    <tr>
+                       <td height="38"> </td>
+                       <td><input  type="submit" value="Ir a Consulta" style='width:170px; height:25px' /></td>
+
+                    </tr>
+                  
+               </tbody>
+           </table>
+
+ </FORM> 
+                             </td>
+                             <td height="350" width="500"></td>
+                              </tr>
+                              </tbody>
+            </table>
+
+      </div>
+              </div>
+       
+   </body>
+    <div id="footer"> <a href=""><img src="../imagenes/footer.png" /></a></div>
+    
+</html>
---
+++ proyectoafp/Administrador/Busca_nombre_AFP.php
@@ -0,0 +1,81 @@
+<?php
+include("../conexion.php");
+   // $result = "";
+    $conn = dbConnect();
+    //$combobox;
+
+ echo $afp=$_POST['afp']; 
+ 
+ ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Registro</title>
+         <?php
+              include('header.php');
+         ?>
+    </head>
+    <body>
+         <div id="page-wrap">
+        <div id="contenedor">  
+                <?php
+                    include('menu_administrador.php');
+                ?>
+      <div>
+           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <a href="../Salir.php">Salir</a>
+       </div>
+  <table border="0" >
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                 <tbody>
+                    <tr>
+                         <td height="350" width="500"></td>
+                         <td height="350" width="700">
+                             <FORM action="ficha.php" method="post"> 
+<h1></h1>  
+<table border="0" align="center" bgcolor="#D0D0D0" >
+               <thead>
+                   <tr>
+                       <th height="65"><font color="#CC0000">Busqueda de cliente por AFP</font></th>
+                       <th></th>
+                   </tr>
+               </thead>
+               <tbody>
+                 <tr>
+                       <td height="38"><font color="#666666">seleccione nombre</font></td>
+                       <td><select name="nombre_seleccionado">
+                               <OPTION VALUE="0">Seleccione Nombre</OPTION>
+                        <?php        $result2 = mysqli_query($conn,"SELECT * FROM cliente  WHERE afp=$afp ORDER BY nombres ASC");
+                        
+                             while ($row2=mysqli_fetch_array($result2)){ $nombre_apellido=$row2['nombres']." ".$row2['apellidos']?> <OPTION VALUE="<?php echo $row2['rut'] ?>"><?php echo strtoupper($nombre_apellido) ?></OPTION><?php } ?> 
+                    </select></td>
+                   </tr>
+                    
+                    <tr>
+                       <td height="38"> </td>
+                       <td><input  type="submit" value="Ir a Consulta" style='width:170px; height:25px' /></td>
+
+                    </tr>
+                  
+               </tbody>
+           </table>
+
+ </FORM> 
+                             </td>
+                             <td height="350" width="500"></td>
+                              </tr>
+                              </tbody>
+            </table>
+
+      </div>
+              </div>
+       
+   </body>
+    <div id="footer"> <a href=""><img src="../imagenes/footer.png" /></a></div>
+    
+</html>
