aa774614a438bd56c3bc87a6fbaa99bd00e92512
claudiadela7 <claudiadela7@gmail.com>
2014-03-06 06:39:17 +0000

    nombres intentando escribir solo letras

--- proyectoafp/registrate.php
+++ proyectoafp/registrate.php
@@ -21,7 +21,16 @@ include("conexion.php");
          ?>
 <script type="text/javascript">
 
-
+function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz";
+  
+        if(letras.indexOf(tecla) == -1){
+         return false;
+        }
+   
+}
     </script>
     </head>
     <body>
@@ -63,7 +72,7 @@ include("conexion.php");
                    
                    <tr>
                        <td height="34"><font color="#666666">Nombres</font></td>
-                       <td><input type="text" name="nombre1"/><input type="text" name="nombre2"/></td>
+                       <td><input type="text" name="nombre1" onkeypress="return soloLetras(event);"/><input type="text" name="nombre2" onkeypress="return soloLetras(event);"/></td>
                    </tr>
                    <tr>
                        <td height="34"><font color="#666666">Apellidos</font></td>
