2f74c6826c47f9dd313de3fc847ce3b7d96c51de
claudiadela7@gmail.com <claudiadela7@gmail.com@dadba06a-a8da-258c-a08a-cfbe5ec56a57>
2014-03-06 06:03:42 +0000

    rut con solo numeros

--- trunk/proyectoafp/registrate.php
+++ trunk/proyectoafp/registrate.php
@@ -20,36 +20,18 @@ include("conexion.php");
               include('header.php');
          ?>
 <script type="text/javascript">
-    function justNumbers(e)
-        {
-        var keynum = window.event ? window.event.keyCode : e.which;
-        if ((keynum == 8) || (keynum == 45))
-        return true;
-         
-        return /\d/.test(String.fromCharCode(keynum));
-        }
-        
-function valida(f) {
-  var ok = true;
-  cajita = document.getElementById("lic");
-  var msg = "Debes escribir algo en los campos:\n";
-  if(f.elements[0].value == ""){msg += "-rut\n";ok = false;}
-  if(f.elements["nombre1"].value == ""){msg += "- nombre1\n";ok = false;}
-  if(f.elements["nombre2"].value == ""){msg += "- nombre2\n";ok = false;}
-  if(f.elements["apellido1"].value == ""){msg += "- primer apellido\n";ok = false;}
-  if(f.elements["apellido2"].value == ""){msg += "- segundo apellido\n";ok = false;}
-  if(f.elements["fecha_nacimiento"].value == ""){msg += "- fecha nacimiento\n";ok = false;}
-  if(f.elements["tipo_cliente"].value == "0"){msg += "- tipo de cliente\n";ok = false;}
-  if(f.elements["region_residencia"].value == "0"){msg += "- region de residencia\n";ok = false;}
-  if(f.elements["afp"].value == "0"){msg += "- Nombre AFP\n";ok = false;}
-  if(cajita.checked==false){msg+="- Debe aceptar los términos y condiciones"; ok=false;}
-  if(ok == false){alert(msg);}return ok;}
+
+
     </script>
     </head>
     <body>
          <div id="page-wrap">
         <div id="contenedor">  
                 <?php
+                  echo '<script type="text/javascript">';
+                    include('js/valida.js');
+                    include('js/solonumeros.js'); 
+                echo '</script>';
                     include('menu.php');
                 ?>
             
