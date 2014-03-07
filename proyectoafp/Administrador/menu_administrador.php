698ba60120e6b64bc67310921a1429e35d1aa4b7
claudiadela7 <claudiadela7@gmail.com>
2014-03-07 15:55:29 +0000

    cambio en el menu administrador. agregada vista para aceptar a los usuarios registrados

--- proyectoafp/Administrador/menu_administrador.php
+++ proyectoafp/Administrador/menu_administrador.php
@@ -4,6 +4,7 @@
           <ul>
               <li><a href="home.php">Home</a></li>
           <li><a href="buscar_cliente.php">Buscar Cliente</a></li>
+          <li><a href="Aceptar_usuarios.php">Aceptar usuarios</a></li>
            </ul>
         
 
