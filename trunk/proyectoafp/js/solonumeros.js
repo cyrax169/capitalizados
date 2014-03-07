44b6a419fa8fe5b9d2fd436239e384f76cb6e389
claudiadela7@gmail.com <claudiadela7@gmail.com@dadba06a-a8da-258c-a08a-cfbe5ec56a57>
2014-03-07 15:41:56 +0000

    cambio en la validacion que permite solo la entrada de numeros para que permita la entrada de  K.

--- trunk/proyectoafp/js/solonumeros.js
+++ trunk/proyectoafp/js/solonumeros.js
@@ -1,7 +1,7 @@
     function justNumbers(e)
         {
         var keynum = window.event ? window.event.keyCode : e.which;
-        if ((keynum == 8) || (keynum == 45))
+        if ((keynum == 107) || (keynum == 45))
         return true;
          
         return /\d/.test(String.fromCharCode(keynum));
