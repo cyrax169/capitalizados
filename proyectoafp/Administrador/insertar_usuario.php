0f01477cd02adb80594613c93aabc2b9d79fbf71
claudiadela7 <claudiadela7@gmail.com>
2014-03-07 19:52:30 +0000

    envio de clave por email

--- proyectoafp/Administrador/insertar_usuario.php
+++ proyectoafp/Administrador/insertar_usuario.php
@@ -22,13 +22,42 @@ $contrasena1=md5($contrasena);
 
 mysqli_query($conn,"INSERT INTO usuario (rut,contrasena,tipo) VALUES ('$rut','$contrasena1','1');");
  
-
+$datos = mysqli_query($conn,"SELECT  * FROM cliente WHERE rut='$rut'");
+    
+   while($row2=mysqli_fetch_array($datos)){
+       $nombre=$row2['nombres']; 
+       $apellido=$row2['apellidos'];
+       $telefono=$row2['telefono'];
+       $email=$row2['email'];
+       
+   }
 $strSQL = "update cliente set cliente_activo = '1' where rut='$rut';";
     mysqli_query($conn,$strSQL);
     mysqli_close($conn);
 
 ?> 
+<?php
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "cyrax169@gmail.com";
+$email_subject = "Contacto desde el sitio web";
+$coment="Bienvenido a Capitalizados su clave de acceso es:".$contrasena;
 
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $nombre . "\n";
+$email_message .= "Apellido: " . $apellido . "\n";
+$email_message .= "E-mail: " . $email . "\n";
+$email_message .= "Teléfono: " . $telefono . "\n";
+$email_message .= "Comentarios: " . $coment . "\n\n";
+
+$email_from=$_POST['email'];
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+?>
 
 <html>
     <head>
@@ -54,14 +83,10 @@ $strSQL = "update cliente set cliente_activo = '1' where rut='$rut';";
                     <table border="" >
                         <tbody align="center">
                             <tr>
-                                <td></td>
-                                <td></td>
+                                <td> Contraseña enviada a nuevo Cliente</td>
+                              
                             </tr>
                             
-                            <tr>
-                                <td></td>
-                                <td></td>
-                            </tr>
                         </tbody>
                     </table>
                 </form>
@@ -71,9 +96,4 @@ $strSQL = "update cliente set cliente_activo = '1' where rut='$rut';";
         <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>     
 </html><?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
