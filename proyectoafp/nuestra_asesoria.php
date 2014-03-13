75b4a4968362b08d5ca454493c88bea5d1d073b9
claudiadela7 <claudiadela7@gmail.com>
2014-03-13 12:25:07 +0000

    vista asesoria creada y lista

---
+++ proyectoafp/nuestra_asesoria.php
@@ -0,0 +1,102 @@
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Asesoría</title>
+         <?php
+              include('header.php');
+         ?>
+    </head>
+    <body>
+        <div id="page-wrap">
+        <div id="contenedor">  
+                <?php
+                    include('menu.php');
+                ?>
+
+       </div>
+            <CENTER>
+            <table  border="0">
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody align="center">
+                    <tr ><td height="300" width="290"><table border="0" align="center" bgcolor='#DFD5FF'>
+                                <tbody>
+                                    <tr>
+                                        <td height="500" >
+                                <ul  style="list-style-position: outside">
+                                    <li style="height: 50px"><a href="" style="font-size: 12"> POR QUÉ INCLINARSE POR NUESTRA ASESORÍA </a></li>
+                                <li style="height: 50px"><a href="" style="font-size: 12"> SERVICIOS OFRECIDOS</a></li>
+                                <li style="height: 50px"><a href="" style="font-size: 12"> BENEFICIOS ADICIONALES DE UNA ESTRATÉGIA ACTIVA</a></li>
+                                <li style="height: 50px"><a href="" style="font-size: 12">PROCEDIMIENTO DE CÁLCULO DE LOS HONORARIOS DE LA ASESORÍA</a></li>
+                                <li style="height: 50px"><a href="" style="font-size: 12"> INFORMACIÓN RELEVANTE PARA LOS POTENCIALES CLIENTES</a></li>
+                                </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                     </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td height="300" width="360"><table border="0" align="center">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody align="center">
+                                    <tr>
+                                        <td height="100"><img src="imagenes/Resumen_Ene-Feb2014.PNG" border="0" width="360" height="300" align="center"/></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td height="300" width="300"><table align="center" border="0">
+                                <thead>
+                                    <tr>
+                                        <th><font color="#CC0000"> POR QUÉ INCLINARSE POR  </font></th>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <th><font color="#CC0000"> NUESTROS SERVICIOS</font></th>
+                                        
+                                    </tr>
+                                </thead>
+                                <tbody>                                                                    
+                                    <tr>
+                                        <td height="230">
+                                           <br> Porque <font color="#CC0000"> CAPITALIZADOS</font> es una opción efec-<br>
+                                            tiva para aumentar el monto del fondo de<br>
+                                            pensiones. Para ello, nuestro departamento de<br>
+                                            análisis de mercados e inversiones desafía la <br>
+                                            rentabilidad que otorga el sistema de AFP, a <br>
+                                            través de la entrega de sugerencias para la toma <br>
+                                            de decisiones de inversión en los cinco fondos posibles.<br><br>
+                                            Porque <font color="#CC0000">CAPITALIZADOS</font> dispone de <br>  
+                                            estrategias de inversión estratificadas, según las <br>
+                                            características previsionales y etarias de los clientes.<br>
+                                            Porque <font color="#CC0000">CAPITALIZADOS</font> usa el sistema <br>
+                                            ganar-ganar, es decir, la asesoría tiene costos si <br> 
+                                            y solo si, las sugerencias superen la rentabilidad <br>
+                                            del fondo original del cliente.<br><br>
+                                            Porque los resultados de las estrategias de <font color="#CC0000">CAPITALIZADOS</font> lo demuestran
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                         
+                    </tr>
+                 
+                </tbody>
+            </table>
+</CENTER>
+            </div>
+        
+   </body>
+   <div id="footer"> <a href=""><img src="imagenes/footer.png" /></a></div>
+</html>
